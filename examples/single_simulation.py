d230c3a8d0133d4b5faf6db58388d7dee4bd818f
sgbaird <sterling.baird@icloud.com>
2022-03-28 20:56:05 -0600

    Update single_simulation.py

--- examples/single_simulation.py
+++ examples/single_simulation.py
@@ -4,9 +4,15 @@ from boppf.utils.particle_packing import particle_packing_simulation
 dummy = True
 
 if dummy:
-    id = "tmp"
+    uid = "tmp"
     particles = 100
 else:
     particles = 1500000
 
-particle_packing_simulation(id, particles)
+means = [120, 120, 120]
+stds = [10, 10, 10]
+fractions = [0.33, 0.33]
+
+vol_frac = particle_packing_simulation(
+    uid=uid, particles=particles, means=means, stds=stds, fractions=fractions
+)
