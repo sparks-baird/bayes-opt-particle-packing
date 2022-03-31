e7f203222f1ec7b1676fbe4e9f508b0dda371f10
sgbaird <sterling.baird@icloud.com>
2022-03-30 21:58:52 -0600

    cpu is faster, probably a bug (?)

--- examples/boppf_example.py
+++ examples/boppf_example.py
@@ -1,4 +1,5 @@
 """Reproduce paper results."""
+from psutil import cpu_count
 import torch
 from boppf.boppf import BOPPF
 from boppf.utils.data import load_data
@@ -10,23 +11,31 @@ X_train, y_train = load_data(fname=fname, folder=data_dir)
 
 dummy = False
 
-device_str = "cuda"  # "cpu"
+device_str = "cpu"  # "cpu"
 
 if dummy:
-    n_sobol = 2
-    n_bayes = 3
+    n_sobol = 5
+    n_bayes = 100 - 5
     particles = 100
 else:
-    n_sobol = 16
-    n_bayes = 100 - 16
-    particles = int(1.5e6)
+    X_train = X_train.head(2000)
+    y_train = y_train.head(2000)
+    n_sobol = 5
+    n_bayes = 100 - 5
+    particles = 1000
+    # n_sobol = 16
+    # n_bayes = 100 - 16
+    # particles = int(1.5e6)
+
+# save one CPU for my poor machine
+max_parallel = max(1, cpu_count(logical=False) - 1)
 
 boppf = BOPPF(
     dummy=dummy,
     n_sobol=n_sobol,
     n_bayes=n_bayes,
     particles=particles,
-    include_logical_cores=False,
+    max_parallel=max_parallel,
     torch_device=torch.device(device_str),
 )
 
