978e33ffba16f3782af29e4d97349fdf8dee4170
sgbaird <sterling.baird@icloud.com>
2022-04-15 23:25:42 -0600

    loop through multiple seeds

--- examples/paper_results.py
+++ examples/paper_results.py
@@ -16,8 +16,6 @@ dummy = True
 device_str = "cuda"  # "cuda" or "cpu"
 use_saas = False
 
-random_seed = 10
-
 if dummy:
     # https://stackoverflow.com/questions/49529372/force-gpu-memory-limit-in-pytorch
     # torch.cuda.set_per_process_memory_fraction(0.25, "cuda")
@@ -29,7 +27,8 @@ if dummy:
     X_train = X_train.head(n_train_keep)
     y_train = y_train.head(n_train_keep)
     max_parallel = 2
-    debug = False
+    debug = True
+    random_seeds = range(10, 12)
 else:
     n_sobol = 10
     n_bayes = 100 - n_sobol
@@ -40,6 +39,7 @@ else:
     # save one CPU for my poor, overworked machine
     max_parallel = max(1, cpu_count(logical=False) - 1)
     debug = False
+    random_seeds = range(10, 21)
 
 combs = list(product([True, False], repeat=3))
 
@@ -51,32 +51,32 @@ keys = [
 
 for comb in combs:
     kwargs = {k: v for k, v in zip(keys, comb)}
+    for seed in random_seeds:
+        pprint(kwargs)
+        print("seed: ", seed)
+        # kwargs = dict(
+        #     remove_scaling_degeneracy=False,
+        #     remove_composition_degeneracy=True,
+        #     use_order_constraint=False,
+        # )
+        boppf = BOPPF(
+            dummy=dummy,
+            n_sobol=n_sobol,
+            n_bayes=n_bayes,
+            particles=particles,
+            max_parallel=max_parallel,
+            torch_device=torch.device(device_str),
+            use_saas=use_saas,
+            data_augmentation=False,
+            debug=debug,
+            seed=seed,
+            **kwargs,
+        )
 
-    pprint(kwargs)
-
-    # kwargs = dict(
-    #     remove_scaling_degeneracy=False,
-    #     remove_composition_degeneracy=True,
-    #     use_order_constraint=False,
-    # )
-    boppf = BOPPF(
-        dummy=dummy,
-        n_sobol=n_sobol,
-        n_bayes=n_bayes,
-        particles=particles,
-        max_parallel=max_parallel,
-        torch_device=torch.device(device_str),
-        use_saas=use_saas,
-        data_augmentation=False,
-        debug=debug,
-        seed=random_seed,
-        **kwargs,
-    )
-
-    t0 = time()
-    best_parameters, means, covariances, ax_client = boppf.optimize(
-        X_train, y_train, return_ax_client=True
-    )
-    print("elapsed (s): ", time() - t0)
+        t0 = time()
+        best_parameters, means, covariances, ax_client = boppf.optimize(
+            X_train, y_train, return_ax_client=True
+        )
+        print("elapsed (s): ", time() - t0)
 
 1 + 1
