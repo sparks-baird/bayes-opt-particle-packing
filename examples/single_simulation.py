dc5f9c4b03dabe67ea7782288eaddd7dfee45a44
sgbaird <sterling.baird@icloud.com>
2022-04-29 01:27:16 -0600

    Update single_simulation.py

--- examples/single_simulation.py
+++ examples/single_simulation.py
@@ -10,11 +10,15 @@ if dummy:
     particles = 10
 else:
     uid = str(uuid4())[0:8]
-    particles = 1500000
+    particles = int(2.5e5)
 
 means = [120.0, 120.0, 120.0]
 stds = [10.0, 10.0, 10.0]
 fractions = [0.33, 0.33]
+
+# means = [26.6259030268466, 343.511344840641, 10.0]
+# stds = [173.089078783001, 606.693903338546, 606.693903338546]
+# fractions = [0.252296349186048, 0.747703650813951, 0.0]
 
 cwd = getcwd()
 chdir("boppf/utils")
