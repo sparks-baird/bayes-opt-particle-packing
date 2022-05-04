7746efbe8797c2c0f923fc89fe87239007684b4c
sgbaird <sterling.baird@icloud.com>
2022-05-03 22:51:13 -0600

    Create plot_distribution_example.py

---
+++ examples/plot_distribution_example.py
@@ -0,0 +1,12 @@
+from boppf.utils.plotting import plot_distribution
+
+
+means = [3.0, 4.0, 5.0]
+stds = [0.5, 1.0, 0.75]
+fractions = [0.5, 0.2, 0.3]
+
+fig = plot_distribution(means, stds, fractions)
+fig.show()
+
+1 + 1
+
