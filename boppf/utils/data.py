4a933272f77b19f617716e3f17093ebeec551d42
sgbaird <sterling.baird@utah.edu>
2022-07-07 14:01:42 -0600

    running_size starts at 2 to avoid multiply error

--- boppf/utils/data.py
+++ boppf/utils/data.py
@@ -167,7 +167,7 @@ def gen_symmetric_trials(data, component_slot_names, composition_slot_names):
 
 
 def get_s_mode_radii(size, s, scale, max_running_size=10000):
-    running_size = 1
+    running_size = 2
     n_radii = 0
     s_mode_radii = None
     while n_radii <= size and running_size <= max_running_size:
